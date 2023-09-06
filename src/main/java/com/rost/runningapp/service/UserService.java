3661f21f033b85657330b47fb7550f579c397d59
LRostislav <rostislav.lysenkov24@gmail.com>
2023-09-06 13:54:55 +0300

    add registration

--- src/main/java/com/rost/runningapp/service/UserService.java
+++ src/main/java/com/rost/runningapp/service/UserService.java
@@ -1,7 +1,13 @@
 package com.rost.runningapp.service;
 
 import com.rost.runningapp.dto.RegistrationDto;
+import com.rost.runningapp.models.UserEntity;
+import org.springframework.stereotype.Service;
 
 public interface UserService {
     void saveUser(RegistrationDto registrationDto);
+
+    UserEntity findByEmail(String email);
+
+    UserEntity findByUsername(String username);
 }
--- src/main/java/com/rost/runningapp/service/impl/UserServiceImpl.java
+++ src/main/java/com/rost/runningapp/service/impl/UserServiceImpl.java
@@ -7,9 +7,11 @@ import com.rost.runningapp.repository.RoleRepository;
 import com.rost.runningapp.repository.UserRepository;
 import com.rost.runningapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 
+@Service
 public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
     private RoleRepository roleRepository;
@@ -29,5 +31,15 @@ public class UserServiceImpl implements UserService {
         Role role = roleRepository.findByName("USER");
         user.setRoles(Arrays.asList(role));
         userRepository.save(user);
+    }
+
+    @Override
+    public UserEntity findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public UserEntity findByUsername(String username) {
+        return userRepository.findByUsername(username);
     }
 }
